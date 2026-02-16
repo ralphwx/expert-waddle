9e95edad9945db61cb875b7b5affc0aa75d8c2eb
sewx <cunyuan.wang@lqsilicon.com>
2026-02-16 16:12:34 -0500

    Auto build

--- src/content.js
+++ src/content.js
@@ -1,21 +1,4 @@
-const events = [
-    {
-        "name": "Test event",
-        "links": [
-            "www.example.com/test_event"
-        ],
-        "organizers": [
-            "CML Cornell"
-        ],
-        "date": "2026/03/01",
-        "time": "00:00",
-        "location": "Day Hall, Cornell",
-        "type": "Social",
-        "description": "This event listing is for testing purposes only, and is not a real event"
-    }
-];
-
+const events = [];
 const event_types = ["Protest", "Social", "Education", "Charity", "Meet & Greet", "Other"];
 const organizers = ["CML Cornell", "Flock Off Ithaca", "Kaleidescope House", "Sunrise Ithaca", "Cornell Grad Students United", "Tompkins County Workers Center", "Ithaca Tenants Union", "Students for a Democratic Cornell"];
-
-export {events, event_types, organizers};
+export {events, event_types, organizers}
