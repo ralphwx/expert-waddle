364ce88377958276ad31b92cf578183681785f6c
sewx <cunyuan.wang@lqsilicon.com>
2026-02-17 13:01:45 -0500

    Auto scrape and build

--- src/content.js
+++ src/content.js
@@ -1,19 +1,5 @@
-const events = [
-    {
-        "name": "Sample event",
-        "links": [
-            "www.example.com/sample/event"
-        ],
-        "organizers": [
-            "CML Cornell"
-        ],
-        "date": "2026/03/01",
-        "time": "00:00",
-        "location": "Day Hall, Cornell",
-        "type": "Social",
-        "description": "This is a test event listing, not a real event."
-    }
-];
+const events = [];
 const event_types = ["Protest", "Social", "Education", "Charity", "Meet & Greet", "Other"];
 const organizers = ["CML Cornell", "Flock Off Ithaca", "Kaleidescope House", "Sunrise Ithaca", "Cornell Grad Students United", "Tompkins County Workers Center", "Ithaca Tenants Union", "Students for a Democratic Cornell"];
-export {events, event_types, organizers}
+const last_updated = 2026-02-16
+export {events, event_types, organizers, last_updated}
