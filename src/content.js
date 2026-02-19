043b94e24e2216b3164bf82656beb876cc144ce5
sewx <cunyuan.wang@lqsilicon.com>
2026-02-19 10:43:59 -0500

    Manual build

--- src/content.js
+++ src/content.js
@@ -1,5 +1,5 @@
 const events = [];
 const event_types = ["Protest", "Social", "Education", "Charity", "Meet & Greet", "Other"];
 const organizers = ["CML Cornell", "Flock Off Ithaca", "Kaleidescope House", "Sunrise Ithaca", "Cornell Grad Students United", "Tompkins County Workers Center", "Ithaca Tenants Union", "Students for a Democratic Cornell"];
-const last_updated = 2026-02-16
+const last_updated = "2026-02-16";
 export {events, event_types, organizers, last_updated}
