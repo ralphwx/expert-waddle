ebbb0a23b58f7daeee80fce3559f771c414216c2
ralphwx <119894249+ralphwx@users.noreply.github.com>
2026-04-15 15:25:24 -0400

    Fixed ordering, missing org

--- src/content.js
+++ src/content.js
@@ -2,12 +2,12 @@ const events = [
     {
         "name": "Hands off our Democracy!  Weekly Honk and Wave",
         "links": [
-            "https://indivisibletompkins.org/event/hands-off-our-democracy-weekly-honk-and-wave-2/2026-05-20/"
+            "https://indivisibletompkins.org/event/hands-off-our-democracy-weekly-honk-and-wave-2/2026-04-15/"
         ],
         "organizers": [
             "Indivisible Tompkins"
         ],
-        "date": "2026/05/20",
+        "date": "2026/04/15",
         "time": "16:30",
         "location": "West Clinton &#038; South Meadow Intersection - 401 S Meadow St",
         "type": "Protest",
@@ -15,121 +15,17 @@ const events = [
     },
     {
         "name": "Weekly Honk and Wave to Protect Democracy",
-        "links": [],
-        "organizers": [],
-        "date": "2026/05/16",
-        "time": "12:00",
-        "location": "Bernie Milton Pavillion - Bernie Milton Pavilion",
-        "type": "Social",
-        "description": "&lt;p&gt;Please join us for a weekly honk and wave to protect Democracy and Justice in America. We stand on the sidewalk of Seneca Street at the corner of Tioga with our signs as traffic drives by. We speak to people who are interested in our cause and politely encourage them to join us. We have [&hellip;]&lt;/p&gt;\\n"
-    },
-    {
-        "name": "Hands off our Democracy!  Weekly Honk and Wave",
-        "links": [],
-        "organizers": [],
-        "date": "2026/05/13",
-        "time": "16:30",
-        "location": "West Clinton &#038; South Meadow Intersection - 401 S Meadow St",
-        "type": "Protest",
-        "description": "&lt;p&gt;Meet at the north side of W. Clinton at the intersection of S. Meadow. We\u2019ll Line S. Meadow both sides of the street heading south. Wear warm clothes and a safety vest if you have one. Signs with fewer words, LARGE and BOLD, and in contrasting color combinations, are more visible to motorists going by. [&hellip;]&lt;/p&gt;\\n"
-    },
-    {
-        "name": "Weekly Honk and Wave to Protect Democracy",
-        "links": [],
-        "organizers": [],
-        "date": "2026/05/09",
-        "time": "12:00",
-        "location": "Bernie Milton Pavillion - Bernie Milton Pavilion",
-        "type": "Protest",
-        "description": "&lt;p&gt;Please join us for a weekly honk and wave to protect Democracy and Justice in America. We stand on the sidewalk of Seneca Street at the corner of Tioga with our signs as traffic drives by. We speak to people who are interested in our cause and politely encourage them to join us. We have [&hellip;]&lt;/p&gt;\\n"
-    },
-    {
-        "name": "Hands off our Democracy!  Weekly Honk and Wave",
-        "links": [],
-        "organizers": [],
-        "date": "2026/05/06",
-        "time": "16:30",
-        "location": "West Clinton &#038; South Meadow Intersection - 401 S Meadow St",
-        "type": "Protest",
-        "description": "&lt;p&gt;Meet at the north side of W. Clinton at the intersection of S. Meadow. We\u2019ll Line S. Meadow both sides of the street heading south. Wear warm clothes and a safety vest if you have one. Signs with fewer words, LARGE and BOLD, and in contrasting color combinations, are more visible to motorists going by. [&hellip;]&lt;/p&gt;\\n"
-    },
-    {
-        "name": "Weekly Honk and Wave to Protect Democracy",
-        "links": [],
-        "organizers": [],
-        "date": "2026/05/02",
-        "time": "12:00",
-        "location": "Bernie Milton Pavillion - Bernie Milton Pavilion",
-        "type": "Social",
-        "description": "&lt;p&gt;Please join us for a weekly honk and wave to protect Democracy and Justice in America. We stand on the sidewalk of Seneca Street at the corner of Tioga with our signs as traffic drives by. We speak to people who are interested in our cause and politely encourage them to join us. We have [&hellip;]&lt;/p&gt;\\n"
-    },
-    {
-        "name": "Ithaca\\'s Climate Activism Calendar",
         "links": [
-            "https://cornellonfire.org/actionsevents/ithacas-climate-activism-calendar"
+            "https://indivisibletompkins.org/event/weekly-honk-and-wave-to-protect-democracy-7/2026-04-18/"
         ],
         "organizers": [
-            "Cornell On Fire"
+            "Indivisible Tompkins"
         ],
-        "date": "2026/04/30",
-        "time": "15:00",
-        "location": "Online",
-        "type": "Other",
-        "description": "Stay abreast of ongoing actions for climate justice locally with the Google Calendar."
-    },
-    {
-        "name": "Hands off our Democracy!  Weekly Honk and Wave",
-        "links": [],
-        "organizers": [],
-        "date": "2026/04/29",
-        "time": "16:30",
-        "location": "West Clinton &#038; South Meadow Intersection - 401 S Meadow St",
-        "type": "Protest",
-        "description": "&lt;p&gt;Meet at the north side of W. Clinton at the intersection of S. Meadow. We\u2019ll Line S. Meadow both sides of the street heading south. Wear warm clothes and a safety vest if you have one. Signs with fewer words, LARGE and BOLD, and in contrasting color combinations, are more visible to motorists going by. [&hellip;]&lt;/p&gt;\\n"
-    },
-    {
-        "name": "Monthly Meeting",
-        "links": [
-            "https://cornellonfire.org/actionsevents/monthly-meeting"
-        ],
-        "organizers": [
-            "Cornell On Fire"
-        ],
-        "date": "2026/04/25",
-        "time": "14:00",
-        "location": "St Luke Lutheran Church in Collegetown",
-        "type": "Meet & Greet",
-        "description": "Coffee and cookies in action! Join our monthly meetings at St Luke Lutheran Church in Collegetown, typically the last Saturday of the month. Next up: 2-4pm on Saturday 4/25. We especially welcome new members and coalition partners to our monthly meetings!"
-    },
-    {
-        "name": "Weekly Honk and Wave to Protect Democracy",
-        "links": [],
-        "organizers": [],
-        "date": "2026/04/25",
+        "date": "2026/04/18",
         "time": "12:00",
         "location": "Bernie Milton Pavillion - Bernie Milton Pavilion",
-        "type": "Other",
-        "description": "&lt;p&gt;Please join us for a weekly honk and wave to protect Democracy and Justice in America. We stand on the sidewalk of Seneca Street at the corner of Tioga with our signs as traffic drives by. We speak to people who are interested in our cause and politely encourage them to join us. We have [&hellip;]&lt;/p&gt;\\n"
-    },
-    {
-        "name": "Hands off our Democracy!  Weekly Honk and Wave",
-        "links": [],
-        "organizers": [],
-        "date": "2026/04/22",
-        "time": "16:30",
-        "location": "West Clinton &#038; South Meadow Intersection - 401 S Meadow St",
         "type": "Protest",
-        "description": "&lt;p&gt;Meet at the north side of W. Clinton at the intersection of S. Meadow. We\u2019ll Line S. Meadow both sides of the street heading south. Wear warm clothes and a safety vest if you have one. Signs with fewer words, LARGE and BOLD, and in contrasting color combinations, are more visible to motorists going by. [&hellip;]&lt;/p&gt;\\n"
-    },
-    {
-        "name": "Bread and Puppet Theater\u2019s The End of the World Never Minding Show",
-        "links": [],
-        "organizers": [],
-        "date": "2026/04/19",
-        "time": "19:00",
-        "location": "Homer Center for the Arts - 72 South Main Street",
-        "type": "Social",
-        "description": "&lt;p&gt;It is time for End of the World Never Minding! Join Bread &amp; Puppet Theater for an urgently-needed new puppet show featuring our upside-down situation, a revolt orchestra, screaming choirs, and a reckoning with the catastrophe of logic.&nbsp; Indivisible Tompkins and Indivisible Cortland County are hosting this show by the renowned&nbsp;Bread and Puppet theater group&nbsp;at [&hellip;]&lt;/p&gt;\\n"
+        "description": "&lt;p&gt;Please join us for a weekly honk and wave to protect Democracy and Justice in America. We stand on the sidewalk of Seneca Street at the corner of Tioga with our signs as traffic drives by. We speak to people who are interested in our cause and politely encourage them to join us. We have [&hellip;]&lt;/p&gt;\\n"
     },
     {
         "name": "Climate Criminals Art Exhibit Social",
@@ -146,20 +42,168 @@ const events = [
         "description": "Join us for a gallery exhibit at Watermargin Coop featuring art-ivism that questions the cultural narrative of \\xe2\\x80\\x9cnormalcy\\xe2\\x80\\x9d glossing climate criminality. "
     },
     {
+        "name": "Bread and Puppet Theater\u2019s The End of the World Never Minding Show",
+        "links": [
+            "https://indivisibletompkins.org/event/bread-and-puppet-theaters-the-end-of-the-world-never-minding-show/"
+        ],
+        "organizers": [
+            "Indivisible Tompkins"
+        ],
+        "date": "2026/04/19",
+        "time": "19:00",
+        "location": "Homer Center for the Arts - 72 South Main Street",
+        "type": "Other",
+        "description": "&lt;p&gt;It is time for End of the World Never Minding! Join Bread &amp; Puppet Theater for an urgently-needed new puppet show featuring our upside-down situation, a revolt orchestra, screaming choirs, and a reckoning with the catastrophe of logic.&nbsp; Indivisible Tompkins and Indivisible Cortland County are hosting this show by the renowned&nbsp;Bread and Puppet theater group&nbsp;at [&hellip;]&lt;/p&gt;\\n"
+    },
+    {
+        "name": "Hands off our Democracy!  Weekly Honk and Wave",
+        "links": [
+            "https://indivisibletompkins.org/event/hands-off-our-democracy-weekly-honk-and-wave-2/2026-04-22/"
+        ],
+        "organizers": [
+            "Indivisible Tompkins"
+        ],
+        "date": "2026/04/22",
+        "time": "16:30",
+        "location": "West Clinton &#038; South Meadow Intersection - 401 S Meadow St",
+        "type": "Protest",
+        "description": "&lt;p&gt;Meet at the north side of W. Clinton at the intersection of S. Meadow. We\u2019ll Line S. Meadow both sides of the street heading south. Wear warm clothes and a safety vest if you have one. Signs with fewer words, LARGE and BOLD, and in contrasting color combinations, are more visible to motorists going by. [&hellip;]&lt;/p&gt;\\n"
+    },
+    {
         "name": "Weekly Honk and Wave to Protect Democracy",
-        "links": [],
-        "organizers": [],
-        "date": "2026/04/18",
+        "links": [
+            "https://indivisibletompkins.org/event/weekly-honk-and-wave-to-protect-democracy-7/2026-04-25/"
+        ],
+        "organizers": [
+            "Indivisible Tompkins"
+        ],
+        "date": "2026/04/25",
         "time": "12:00",
         "location": "Bernie Milton Pavillion - Bernie Milton Pavilion",
-        "type": "Social",
+        "type": "Protest",
+        "description": "&lt;p&gt;Please join us for a weekly honk and wave to protect Democracy and Justice in America. We stand on the sidewalk of Seneca Street at the corner of Tioga with our signs as traffic drives by. We speak to people who are interested in our cause and politely encourage them to join us. We have [&hellip;]&lt;/p&gt;\\n"
+    },
+    {
+        "name": "Monthly Meeting",
+        "links": [
+            "https://cornellonfire.org/actionsevents/monthly-meeting"
+        ],
+        "organizers": [
+            "Cornell On Fire"
+        ],
+        "date": "2026/04/25",
+        "time": "14:00",
+        "location": "St Luke Lutheran Church in Collegetown",
+        "type": "Meet & Greet",
+        "description": "Coffee and cookies in action! Join our monthly meetings at St Luke Lutheran Church in Collegetown, typically the last Saturday of the month. Next up: 2-4pm on Saturday 4/25. We especially welcome new members and coalition partners to our monthly meetings!"
+    },
+    {
+        "name": "Hands off our Democracy!  Weekly Honk and Wave",
+        "links": [
+            "https://indivisibletompkins.org/event/hands-off-our-democracy-weekly-honk-and-wave-2/2026-04-29/"
+        ],
+        "organizers": [
+            "Indivisible Tompkins"
+        ],
+        "date": "2026/04/29",
+        "time": "16:30",
+        "location": "West Clinton &#038; South Meadow Intersection - 401 S Meadow St",
+        "type": "Protest",
+        "description": "&lt;p&gt;Meet at the north side of W. Clinton at the intersection of S. Meadow. We\u2019ll Line S. Meadow both sides of the street heading south. Wear warm clothes and a safety vest if you have one. Signs with fewer words, LARGE and BOLD, and in contrasting color combinations, are more visible to motorists going by. [&hellip;]&lt;/p&gt;\\n"
+    },
+    {
+        "name": "Ithaca\\'s Climate Activism Calendar",
+        "links": [
+            "https://cornellonfire.org/actionsevents/ithacas-climate-activism-calendar"
+        ],
+        "organizers": [
+            "Cornell On Fire"
+        ],
+        "date": "2026/04/30",
+        "time": "15:00",
+        "location": "Online",
+        "type": "Other",
+        "description": "Stay abreast of ongoing actions for climate justice locally with the Google Calendar."
+    },
+    {
+        "name": "Weekly Honk and Wave to Protect Democracy",
+        "links": [
+            "https://indivisibletompkins.org/event/weekly-honk-and-wave-to-protect-democracy-7/2026-05-02/"
+        ],
+        "organizers": [
+            "Indivisible Tompkins"
+        ],
+        "date": "2026/05/02",
+        "time": "12:00",
+        "location": "Bernie Milton Pavillion - Bernie Milton Pavilion",
+        "type": "Protest",
         "description": "&lt;p&gt;Please join us for a weekly honk and wave to protect Democracy and Justice in America. We stand on the sidewalk of Seneca Street at the corner of Tioga with our signs as traffic drives by. We speak to people who are interested in our cause and politely encourage them to join us. We have [&hellip;]&lt;/p&gt;\\n"
     },
     {
         "name": "Hands off our Democracy!  Weekly Honk and Wave",
-        "links": [],
-        "organizers": [],
-        "date": "2026/04/15",
+        "links": [
+            "https://indivisibletompkins.org/event/hands-off-our-democracy-weekly-honk-and-wave-2/2026-05-06/"
+        ],
+        "organizers": [
+            "Indivisible Tompkins"
+        ],
+        "date": "2026/05/06",
+        "time": "16:30",
+        "location": "West Clinton &#038; South Meadow Intersection - 401 S Meadow St",
+        "type": "Protest",
+        "description": "&lt;p&gt;Meet at the north side of W. Clinton at the intersection of S. Meadow. We\u2019ll Line S. Meadow both sides of the street heading south. Wear warm clothes and a safety vest if you have one. Signs with fewer words, LARGE and BOLD, and in contrasting color combinations, are more visible to motorists going by. [&hellip;]&lt;/p&gt;\\n"
+    },
+    {
+        "name": "Weekly Honk and Wave to Protect Democracy",
+        "links": [
+            "https://indivisibletompkins.org/event/weekly-honk-and-wave-to-protect-democracy-7/2026-05-09/"
+        ],
+        "organizers": [
+            "Indivisible Tompkins"
+        ],
+        "date": "2026/05/09",
+        "time": "12:00",
+        "location": "Bernie Milton Pavillion - Bernie Milton Pavilion",
+        "type": "Protest",
+        "description": "&lt;p&gt;Please join us for a weekly honk and wave to protect Democracy and Justice in America. We stand on the sidewalk of Seneca Street at the corner of Tioga with our signs as traffic drives by. We speak to people who are interested in our cause and politely encourage them to join us. We have [&hellip;]&lt;/p&gt;\\n"
+    },
+    {
+        "name": "Hands off our Democracy!  Weekly Honk and Wave",
+        "links": [
+            "https://indivisibletompkins.org/event/hands-off-our-democracy-weekly-honk-and-wave-2/2026-05-13/"
+        ],
+        "organizers": [
+            "Indivisible Tompkins"
+        ],
+        "date": "2026/05/13",
+        "time": "16:30",
+        "location": "West Clinton &#038; South Meadow Intersection - 401 S Meadow St",
+        "type": "Protest",
+        "description": "&lt;p&gt;Meet at the north side of W. Clinton at the intersection of S. Meadow. We\u2019ll Line S. Meadow both sides of the street heading south. Wear warm clothes and a safety vest if you have one. Signs with fewer words, LARGE and BOLD, and in contrasting color combinations, are more visible to motorists going by. [&hellip;]&lt;/p&gt;\\n"
+    },
+    {
+        "name": "Weekly Honk and Wave to Protect Democracy",
+        "links": [
+            "https://indivisibletompkins.org/event/weekly-honk-and-wave-to-protect-democracy-7/2026-05-16/"
+        ],
+        "organizers": [
+            "Indivisible Tompkins"
+        ],
+        "date": "2026/05/16",
+        "time": "12:00",
+        "location": "Bernie Milton Pavillion - Bernie Milton Pavilion",
+        "type": "Protest",
+        "description": "&lt;p&gt;Please join us for a weekly honk and wave to protect Democracy and Justice in America. We stand on the sidewalk of Seneca Street at the corner of Tioga with our signs as traffic drives by. We speak to people who are interested in our cause and politely encourage them to join us. We have [&hellip;]&lt;/p&gt;\\n"
+    },
+    {
+        "name": "Hands off our Democracy!  Weekly Honk and Wave",
+        "links": [
+            "https://indivisibletompkins.org/event/hands-off-our-democracy-weekly-honk-and-wave-2/2026-05-20/"
+        ],
+        "organizers": [
+            "Indivisible Tompkins"
+        ],
+        "date": "2026/05/20",
         "time": "16:30",
         "location": "West Clinton &#038; South Meadow Intersection - 401 S Meadow St",
         "type": "Protest",
